eefbe7500fe03e7acc552e26f955497b26fac49d
avibhardwaj243 <avibhardwaj243@gmail.com>
2018-09-18 06:54:20 +0530

    master : route created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,30 @@
 import React, { Component } from 'react';
-
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './containers/Home';
+import Login from './containers/Login';
+import Upload from './containers/Upload';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
+   render() {
+      return (
+         <Router>
+            <div>
+               <h2>Welcome to React Router Tutorial</h2>
+               <ul>
+                  <li><Link to={'/'}>Home</Link></li>
+                  <li><Link to={'/Login'}>Login</Link></li>
+                  <li><Link to={'/Upload'}>Upload</Link></li>
+               </ul>
+               <hr />
 
-		this.state = {
-			imageURL: ''
-		};
-
-		this.handleUploadImage = this.handleUploadImage.bind(this);
-	}
-
-	handleUploadImage(ev) {
-		ev.preventDefault();
-
-		const data = new FormData();
-		data.append('file', this.uploadInput.files[0]);
-		data.append('filename', this.fileName.value);
-
-		fetch('http://localhost:4000/user/upload', {
-			method: 'POST',
-			body: data
-		}).then(response => {
-			response.json().then(body => {
-				this.setState({ imageURL: `http://localhost:4000/${body.file}` });
-			        });
-		        });
-	        }
-
-	render() {
-		return (
-			<div className="App">
-				<h1 className="display-3">FileUploads</h1>
-				<form onSubmit={this.handleUploadImage}>
-					<div>
-						<input multiple ref={ref => {this.uploadInput = ref;}} type="file"/>
-					</div>
-					<br />
-					<div>
-						<input ref={ref => {this.fileName = ref;}} type="text" placeholder="Enter the desired name of file" />
-					</div>
-					<br />
-					<div>
-						<button className="btn btn-success">Upload</button>
-					</div>
-					<hr />
-					<p>Uploaded Image:</p>
-					<img src={this.state.imageURL} alt="img" />
-				</form>
-			</div>
-		);
-	}
+               <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/Login' component={Login} />
+                  <Route exact path='/Upload' component={Upload} />
+               </Switch>
+            </div>
+         </Router>
+      );
+   }
 }
 export default App;
